ecbb4b9bde14ad4707ca26aaab5a04dea6476400
RYZZ <vladimirirr@outlook.com>
2023-05-08 22:29:09 +0800

    update d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,15 @@ type T_CreateAppOptions = {
   tagName: string
   rootComponent: Object
 }
+type T_SilverComponentObject = {
+  name: string
+  initialize: Function
+  props?: string[]
+  emits?: string[]
+  components?: T_SilverComponentObject[]
+}
 
 export const createApp: (options: T_CreateAppOptions) => void
 
-export const attr: (name: string, value: any) => string
+export const toAttr: (name: string, value: any) => string
+export const toComponent: (component: T_SilverComponentObject) => string
